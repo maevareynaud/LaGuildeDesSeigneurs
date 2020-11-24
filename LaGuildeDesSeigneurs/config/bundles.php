99815877ba4a96df251456bb0dac42d9c7c82ced
Maeva Reynaud <maeva.reynaud@hotmail.fr>
2020-11-24 13:47:00 +0100

    ajout de la doc

--- LaGuildeDesSeigneurs/config/bundles.php
+++ LaGuildeDesSeigneurs/config/bundles.php
@@ -8,4 +8,7 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
